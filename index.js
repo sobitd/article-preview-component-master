41682f38d2b86cc5f677d637a84e45c1a783b165
Daniel Sobit <sobitdaniel@gmail.com>
2020-11-22 18:03:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Get the Social Media Pop Up Button
+let popUp=document.getElementById("toggle");
+
+
+// When the user clicks the Share button, visibility CSS class is toggled
+
+toggle.addEventListener("click",function(){
+	document.getElementById("social-link").classList.toggle("visible");
+});
